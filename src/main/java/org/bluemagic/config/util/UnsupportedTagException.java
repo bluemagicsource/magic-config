2b1a100f38e9f7b0e8fd966e339c275411a99d56
Jack Rodriguez <github@jrod.org>
2012-03-03 17:49:22 -0500

    Refactored and cleaned up Tag logic

---
+++ src/main/java/org/bluemagic/config/util/UnsupportedTagException.java
@@ -0,0 +1,14 @@
+package org.bluemagic.config.util;
+
+import org.bluemagic.config.api.Tag;
+import org.bluemagic.config.decorator.TagDecorator;
+
+public class UnsupportedTagException extends RuntimeException {
+
+	private static final long serialVersionUID = 7852234375791195353L;
+
+	public UnsupportedTagException(Class<? extends TagDecorator> callingClass, Class<? extends Tag> expected, Class<?> found) {
+		super(callingClass.getSimpleName() + " expects a tag of type: " + expected.getName() + ", NOT: " + found.getName() + "!");
+	}
+
+}
