e5b7c083283de0b81532c2c9bc91fdacb6f10c7b
Sean Dobberstein <sdobbers@gmail.com>
2012-04-25 19:16:24 -0400

    Updating name in tag factory

--- src/main/java/org/bluemagic/config/factory/TagFactory.java
+++ src/main/java/org/bluemagic/config/factory/TagFactory.java
@@ -10,7 +10,7 @@ public class TagFactory {
 	
 	private static final Log LOG = LogFactory.getLog(TagFactory.class);
 
-	public static String DEFAULT_TAG_PREFIX = "org.bluemagic.config.api.tag.";
+	public static String API_TAG_PREFIX = "org.bluemagic.config.api.tag.";
 	public static String CONFIG_TAG_PREFIX = "org.bluemagic.config.tags.";
 	
 	public Tag build(String className) {
@@ -20,20 +20,20 @@ public class TagFactory {
 		Class<?> clazz = null;
 		Object instance = null;
 		
-		// 	TRY GETTING CLASS WITHOUT ADDING DEFAULT PACKAGE
+		// 	TRY GETTING CLASS WITHOUT ADDING API PACKAGE
 		clazz = ReflectionUtils.classFromName(originalClassName);
 		
 		if (clazz == null) {
-			className = TagFactory.DEFAULT_TAG_PREFIX + StringUtils.capitalize(originalClassName);
+			className = TagFactory.API_TAG_PREFIX + StringUtils.capitalize(originalClassName);
 			
-			// 	TRY GETTING CLASS AFTER ADDING DEFAULT PACKAGE
+			// 	TRY GETTING CLASS AFTER ADDING API PACKAGE
 			clazz = ReflectionUtils.classFromName(className);
 		}
 		
 		if (clazz == null) {
             className = TagFactory.CONFIG_TAG_PREFIX + StringUtils.capitalize(originalClassName);
 			
-			// 	TRY GETTING CLASS AFTER ADDING DEFAULT PACKAGE
+			// 	TRY GETTING CLASS AFTER ADDING CONFIG PACKAGE
 			clazz = ReflectionUtils.classFromName(className);
 		}
 		
