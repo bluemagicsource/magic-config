1a3c746a0a8ce4b042ced064efa340cd0b446d04
Peter Platt <peter.james.platt@gmail.com>
2012-11-16 00:17:19 -0500

    Minor changes to prevent maven from spitting out warnings

--- src/main/java/org/bluemagic/config/location/FileLocationDecoratingWrapper.java
+++ src/main/java/org/bluemagic/config/location/FileLocationDecoratingWrapper.java
@@ -3,7 +3,6 @@ package org.bluemagic.config.location;
 import java.net.URI;
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
--- src/main/java/org/bluemagic/config/transformer/FileSystemTransformer.java
+++ src/main/java/org/bluemagic/config/transformer/FileSystemTransformer.java
@@ -2,7 +2,6 @@ package org.bluemagic.config.transformer;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
